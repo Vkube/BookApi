add30535e90718d96c35910811205a10e8c26968
vkube <sawmati@gmail.com>
2022-02-28 00:10:20 +0300

    add book to entity framework

--- BookApi/Models/ApplicationContext.cs
+++ BookApi/Models/ApplicationContext.cs
@@ -1,7 +1,25 @@
-﻿namespace ApiControllers.Models
+﻿using Microsoft.EntityFrameworkCore;
+namespace ApiControllers.Models
 {
-    public class ApplicationContext
+    public class ApplicationContext:DbContext
     {
-        
+        public DbSet<Book> Books {get; set;} = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();   
+    }
+ protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().HasData(
+                new Book { Id =  Guid.NewGuid().ToString(), Author = "Shildt", Title = "C# 4.0 : полное руководство", Date= new DateTime(2020,02,30) },
+                new Book { Id = Guid.NewGuid().ToString(), Author = "Richter" , Title= "CLR via C# Программирование на платформе Micrisoft .Net Framework 4.5", Date = new DateTime(2022,01,01) },
+                new Book { Id = Guid.NewGuid().ToString(), Author = "Shildt" , Title= "Java, full documentation ", Date = new DateTime(2020,04,31) },
+                new Book { Id = Guid.NewGuid().ToString(), Author = "Freeman " , Title= "Entity Framework Core 2 for Asp.net ", Date = new DateTime(2019,02,31) },
+                new Book { Id = Guid.NewGuid().ToString(), Author = "Freeman " , Title= "Asp. Net Core 3 with example for professional", Date = new DateTime(2022,01,01) }
+        );
+
+    }
+    
     }
 }
--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -2,9 +2,9 @@
 {
     public class Book
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Author { get; set; }
-        public string Name { get; set; }
-        public int Year { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
     }
 }
